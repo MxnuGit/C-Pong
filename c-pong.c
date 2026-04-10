654bba98548933e5dcf2e4bea48617eb522a6ade
MxnuGit <manuelmenghetti0@gmail.com>
2026-04-10 23:55:56 +0200

    Add: Input handling. Now left mace can be moved by pressing UP and DOWN

--- c-pong.c
+++ c-pong.c
@@ -14,22 +14,25 @@ int main() {
 
     // Game Objects
     const Vector2 maceSize = { 20, 150};
+    const int marginY = 20;
 
     Vector2 leftMacePosition = { 15, windowHeight / 2 - 70};
     const Color leftMaceColor = BLUE;
-
+    
     Vector2 rightMacePosition = { windowWidth - 35, windowHeight / 2 - 70};
     const Color rightMaceColor = RED;
-
-
+    
+    
     const int ballRadius = 15;
+    const float ballSpeed = 2.0f;
     Vector2 ballPosition = { windowWidth / 2, windowHeight / 2};
     const Color ballColor = BLACK;
 
     // Gameloop
     while(!WindowShouldClose()) {
         // Input
-
+        if(IsKeyDown(KEY_UP) && leftMacePosition.y > marginY) leftMacePosition.y -= ballSpeed;
+        if(IsKeyDown(KEY_DOWN) && leftMacePosition.y < windowHeight - maceSize.y - marginY) leftMacePosition.y += ballSpeed;
 
         // Update
         
