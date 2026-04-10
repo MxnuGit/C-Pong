157dc395242e4b43c755749b4621fe31fb197b71
MxnuGit <manuelmenghetti0@gmail.com>
2026-04-10 17:59:37 +0200

    Add: Window configuration

--- c-pong.c
+++ c-pong.c
@@ -1,6 +1,28 @@
 #include <raylib.h>
-#include <stdio.h>
 
 int main() {
-    printf("Hello World!");
+    
+    // Window variables
+    const int width = 900;
+    const int height = 600;
+    const char* title = "C-Pong";
+
+    // Window constructor
+    InitWindow(width, height, title);
+
+    // Gameloop
+    while(!WindowShouldClose()) {
+        // Input
+
+
+        // Update
+
+
+        // Draw
+        BeginDrawing();
+
+        ClearBackground(WHITE);
+
+        EndDrawing();
+    }
 }
