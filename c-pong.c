eff86c0aabd10e7c7f2a7aeff1fdea6afdb8a1cb
MxnuGit <manuelmenghetti0@gmail.com>
2026-04-12 18:43:08 +0200

    Add UI labels for scores and title. Also add comments for each parts of important code

--- c-pong.c
+++ c-pong.c
@@ -31,34 +31,59 @@ int main() {
     int ballDirectionX = 1;
     int ballDirectionY = 1;
 
+
+    int leftScore = 0;
+    int rightScore = 0;
+
     // Gameloop
     while(!WindowShouldClose()) {
-        // Input
+        /*
+        * INPUT
+        */ 
+
+        // left mace input handler
         if(IsKeyDown(KEY_W) && leftMacePosition.y > marginY) leftMacePosition.y -= maceSpeed;
         if(IsKeyDown(KEY_S) && leftMacePosition.y + maceSize.y + marginY < windowHeight) leftMacePosition.y += maceSpeed;
 
+        // right mace input handler
         if(IsKeyDown(KEY_UP) && rightMacePosition.y > marginY) rightMacePosition.y -= maceSpeed;
         if(IsKeyDown(KEY_DOWN) && rightMacePosition.y + maceSize.y + marginY < windowHeight) rightMacePosition.y += maceSpeed;
 
-        // Update
-        if(ballPosition.x >= rightMacePosition.x || ballPosition.x <= leftMacePosition.x) {
+        /*
+        * UPDATE
+        */
+
+        // Check if ball went over right mace
+        if(ballPosition.x >= rightMacePosition.x) {
             ballPosition.x = windowWidth / 2;
             ballPosition.y = windowHeight / 2;
+
+            leftScore++;
+        }
+    
+        // Checks if ball went over left mace
+        if(ballPosition.x <= leftMacePosition.x) {
+            ballPosition.x = windowWidth / 2;
+            ballPosition.y = windowHeight / 2;
+
+            rightScore++;
         }
 
-
+        // Normal ball movement every frame
         ballPosition.x += (ballSpeed * ballDirectionX); 
         ballPosition.y += (ballSpeed * ballDirectionY);
 
+        // checks if ball hits X window bounds
         if(ballPosition.x - ballRadius < 0 || ballPosition.x + ballRadius > windowWidth) {
             ballDirectionX *= -1;
         }
 
+        // check if ball hits Y window bounds
         if(ballPosition.y - ballRadius < 0 || ballPosition.y + ballRadius > windowHeight) {
             ballDirectionY *= -1;
         }
 
-        // Mace collision
+        // right mace collision handler
         if(
             ballPosition.x + ballRadius >= rightMacePosition.x &&
             ballPosition.y + ballRadius <= rightMacePosition.y + maceSize.y &&
@@ -67,6 +92,7 @@ int main() {
             ballDirectionX *= -1;
         }
 
+        // left mace collision handler
         if(
             ballPosition.x - ballRadius <= leftMacePosition.x + maceSize.x &&
             ballPosition.y + ballRadius <= leftMacePosition.y + maceSize.y &&
@@ -76,11 +102,22 @@ int main() {
         }
 
 
-        // Draw
+        /*
+        * DRAW
+        */ 
         BeginDrawing();
 
         ClearBackground(BLACK);
 
+        // Draw title on display
+        DrawText("C-Pong", windowWidth / 2 - MeasureText("C-Pong", 30) / 2, 30, 30, GREEN);
+
+        // Draw left score
+        DrawText(TextFormat("%03i", leftScore), 40, 40, 40, BLUE);
+
+        // Draw right score
+        DrawText(TextFormat("%03i", rightScore), windowWidth - 100, 40, 40, RED);
+        
         DrawRectangleV(leftMacePosition, maceSize, leftMaceColor);
         DrawRectangleV(rightMacePosition, maceSize, rightMaceColor);
         DrawCircleV(ballPosition, ballRadius, ballColor);
